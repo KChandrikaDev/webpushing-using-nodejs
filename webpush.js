0c2a0856169ee88a1b3e647294a99bc85f039fe2
KChandrikaDev <k.chandrika.reddys@gmail.com>
2021-06-25 21:52:31 +0530

    intial

--- webpush.js
+++ webpush.js
@@ -3,8 +3,8 @@ const webpush = require('web-push');
 // VAPID keys should only be generated only once.
 const vapidKeys = {
   publicKey:
-    'BBZY7Q3KEtZArAAWMLi_qzWHbH4vAoqPpIXnRhmlUaw0PVs1Kt_2fgLhuaVI5i8MWASBKx3d6W6UoH2U3qChw9U',
-  privateKey: 'CZtf_JUxmXkCKbzwaKedPPO9BFC99U2rk-GUYDbYAa8'
+    'BLVBhcG12DDHvKck3rAvVjjIYoJnpFxKf9Ueo7ICd7YXhV8VBbdoqEySURZ0C2AJ3ZAPUhK5cmPUn3PesEFFr5w',
+  privateKey: 'MGALZ9xhxZh54kjgJRkQfvl16QtEm8emPPAy_6-C9gA'
 };
 
 webpush.setVapidDetails(
